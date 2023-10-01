8ab023f3ac2557c2440d3fe7b69872f4c4b7af8a
Soszka <bsocha1998@gmail.com>
2023-10-01 09:03:45 +0200

    Redux and store.js initialized in App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+import ReactDOM from 'react-dom';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
 import './styles/global.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.querySelector('#root')
 );
 
